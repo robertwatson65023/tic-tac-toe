d06a969d322775dd0bdfa45161548c2848d88236
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-09-26 19:43:29 +0200

    write the code logic

--- src/game.py
+++ src/game.py
@@ -1,6 +1,18 @@
+import pygame
+import sys
+
 class Game:
     def __init__(self) -> None:
         pass
 
     def launch(self):
-        pass
+        pygame.init()
+
+        screen = pygame.display.set_mode((600, 600))
+        pygame.display.set_caption("Tic Tac Toe")
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
