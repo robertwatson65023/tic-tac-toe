f54cb8b4cda9a9cb9f345905aeb1c743e97cd69e
Timothée MARRERO MEYER <143833750+timotheeMM@users.noreply.github.com>
2023-10-09 18:54:44 +0200

    add docstring to the drawing.py file

--- src/game/drawing.py
+++ src/game/drawing.py
@@ -8,6 +8,15 @@ class Drawing:
         pass
 
     def draw_grid(self, screen):
+        """
+        Draw the game grid on the screen.
+
+        Args:
+            screen: The Pygame screen surface.
+
+        This method draws the grid lines on the screen to represent the Tic Tac Toe board.
+        """
+             
         for i in range(1, NB_COLUMN_ROW):
             pygame.draw.line(screen, GRID_COLOR, (i * SCREEN_SIZE // 3, 0), (i * SCREEN_SIZE // 3, SCREEN_SIZE), 5)
 
@@ -15,6 +24,17 @@ class Drawing:
             pygame.draw.line(screen, GRID_COLOR, (0, i * SCREEN_SIZE // 3), (SCREEN_SIZE, i * SCREEN_SIZE // 3), 5)
     
     def draw_pieces(self, screen, board):
+        """
+        Draw the game pieces on the screen.
+
+        Args:
+            screen: The Pygame screen surface.
+            board: The game board representing the positions of the pieces.
+
+        This method draws the game pieces (blue and yellow circles) on the screen based on the
+        board configuration.
+        """
+         
         blue_circle = pygame.transform.scale(pygame.image.load("img/blue.png"), (70, 70))
         yellow_circle = pygame.transform.scale(pygame.image.load("img/yellow.png"), (70, 70))
         size = SCREEN_SIZE / NB_COLUMN_ROW
@@ -31,6 +51,17 @@ class Drawing:
                     screen.blit(yellow_circle, (x + size // 2 - 35, y + size // 2 - 35))
     
     def draw_line(self, screen, start, end):
+        """
+        Draw a winning line on the screen.
+
+        Args:
+            screen: The Pygame screen surface.
+            start: The starting coordinates of the line.
+            end: The ending coordinates of the line.
+
+        This method draws a line connecting the winning pieces.
+        """
+        
         x_start = SCREEN_SIZE // NB_COLUMN_ROW * (start[0] + 1) - SCREEN_SIZE // NB_COLUMN_ROW // 2
         y_start = SCREEN_SIZE // NB_COLUMN_ROW * (start[1] + 1) - SCREEN_SIZE // NB_COLUMN_ROW // 2
 
@@ -40,6 +71,15 @@ class Drawing:
         pygame.draw.line(screen, DARK_BLUE, (x_start, y_start), (x_end, y_end), 5)
     
     def end_screen(self, screen):
+        """
+        Display the end screen.
+
+        Args:
+            screen: The Pygame screen surface.
+
+        This method displays an end screen prompting to play again and waits for the player's input.
+        """
+        
         screen.fill(BACKGROUND_COLOR)
         font = pygame.font.Font("font/Roboto-Regular.ttf", 25)
         text = font.render("Press space key to play again!", True, (255,255,255))
